95263c0c85e84f90a3e3bab47f60cf3ebe67d6a9
realerdemdoruk <erdemdoruk3@gmail.com>
2022-08-28 12:29:08 +0300

    Finish

---
+++ script.js
@@ -0,0 +1,11 @@
+let menu = document.querySelector("nav ul")
+
+let hamburder = document.querySelector("nav .mobile")
+
+
+hamburder.addEventListener("click",function(){
+
+    menu.classList.toggle("show")
+
+
+})
